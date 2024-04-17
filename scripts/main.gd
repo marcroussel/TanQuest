9cddfc0735048496ba35ccd394dad38ebf6b3051
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2024-04-17 22:42:15 +0200

    adding door scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+@export var number_of_keys:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +11,8 @@ func _ready():
 func _process(delta):
 	pass
 
+
+# Fonction déclenchée lorsqu'une clé a été récupérée
+# Incrémentation du nombre de clés
+func _on_key_picked_up(x, y):
+	number_of_keys += 1
