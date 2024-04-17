e8efc23c5f84617868e7d14ac7bb1c14aa5f89c6
TheEngineerOne <verbruggenlucas2@gmail.com>
2024-04-17 03:56:36 +0200

    adding door script

---
+++ Scenes/door.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+class_name Door
+@export var key_x:float = 0
+@export var key_y:float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_key_key_picked_up(x, y):
+	if(key_x == x && key_y == y):
+		self.queue_free()
