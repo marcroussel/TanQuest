4ddcec807f1248e61dfae8b1f98c90551823349c
TheEngineerOne <verbruggenlucas2@gmail.com>
2024-04-18 02:00:54 +0200

    adding maximum and minimum to sinewave function

--- scripts/sinewave.gd
+++ scripts/sinewave.gd
@@ -17,18 +17,25 @@ extends Line2D
 @export var space_between_points : float = 3
 @export var scale_line : float = 0.5
 @export var speed : float = -0.1
-# Called when the node enters the scene tree for the first time.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export_category("Maximum")
+@export var maximum_value : float = 175
+@export var minimum_value : float = -160
+
 func _process(_delta):
 	
 	var time = Time.get_ticks_msec() * speed
 	var array := []
 	for i in range(number_of_points):
-		array.append(Vector2((offset_x+float(i)*space_between_points)*scale_line, 
-		(offset_y+
-		sin(frequency_a*0.01*((offset_x+float(i)*space_between_points)+time))*amplitude_a+
+		var sin_value : float
+		sin_value = (sin(frequency_a*0.01*((offset_x+float(i)*space_between_points)+time))*amplitude_a+
 		sin(frequency_b*0.01*((offset_x+float(i)*space_between_points)+time))*amplitude_b+
 		sin(frequency_c*0.01*((offset_x+float(i)*space_between_points)+time))*amplitude_c
-		)*scale_line))
+		)
+		if(sin_value>maximum_value):
+			sin_value = maximum_value
+		if(sin_value<minimum_value):
+			sin_value= minimum_value
+		array.append(Vector2((offset_x+float(i)*space_between_points)*scale_line, 
+		(offset_y+sin_value)*scale_line))
 	points = array
