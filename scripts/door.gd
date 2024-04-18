4fc9f5055e55c6c7108cee913f4d8c1a6b6c1200
TheEngineerOne <verbruggenlucas2@gmail.com>
2024-04-18 16:01:10 +0200

    making door open when touching the linked key

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,9 +3,13 @@ extends Node2D
 class_name Door
 @export var key_x:float = 0
 @export var key_y:float = 0
+var hitbox:RigidBody2D
+var animation_node:AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hitbox = get_node("DoorHitBox")
+	animation_node = get_node("DoorSprite")
 	pass # Replace with function body.
 
 
@@ -13,7 +17,11 @@ func _ready():
 func _process(delta):
 	pass
 
+func open():
+	hitbox.queue_free()
+	animation_node.play()
+	pass
 
-func _on_key_key_picked_up(x, y):
-	if(key_x == x && key_y == y):
-		pass
+
+func _on_key_picked_up(x, y):
+	open()
