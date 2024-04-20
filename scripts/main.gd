6d22823e911e10fd58232d855e3df57686c9ce36
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2024-04-20 16:56:26 +0200

    hidding main scene elements when popup appears

--- scripts/main.gd
+++ scripts/main.gd
@@ -23,15 +23,29 @@ func _on_key_picked_up(x, y):
 
 # Called when the create_enigma_popup signal has been emitted
 func _on_first_room_door_create_enigma_popup():
+	# Verifying if a popup has not been created
 	if not is_popup_created:
+		
+		# Generating the first enigma popup
 		var first_enigma = enigma_popup.instantiate()
 		add_child(first_enigma)
-		first_enigma.popup_destroyed.connect(_on_popup_destroyed)
 		first_enigma.show()
-		print("enigma_popup created")
+		
+		# Connecting popup_destroyed signal to the appropriate function
+		first_enigma.popup_destroyed.connect(_on_popup_destroyed) 
+
+		# Hiding main node's elements
+		$GameTileMap.hide()
+		$Player.hide()
 		is_popup_created = true
+
 
 # Called when the popup_destroyed signal has been emitted
 func _on_popup_destroyed():
+	
+	# Showing main node's elements
+	$GameTileMap.show()
+	$Player.show()
+	
 	is_popup_created = false
-	print("is_popup_created = false")
+	
